ea204b4bbe34e973e2cd60d01f8abdba5c01d83e
yyl <bhyyl@buaa.edu.cn>
2024-10-30 10:09:52 +0000

    Change gpu num, image size & queries num to launch a run through in low GPU storage device

--- configs/qhdetr.sh
+++ configs/qhdetr.sh
@@ -13,8 +13,8 @@ python -u main.py \
     --epochs 12 \
     --lr_drop 11 \
     --weight_decay 0.0 \
-    --num_queries_one2one 300 \
-    --num_queries_one2many 1500 \
+    --num_queries_one2one 3 \
+    --num_queries_one2many 15 \
     --k_one2many 6 \
     --lambda_one2many 1.0 \
     --dropout 0.0 \
--- datasets/coco.py
+++ datasets/coco.py
@@ -148,7 +148,8 @@ def make_coco_transforms(image_set):
         [T.ToTensor(), T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]
     )
 
-    scales = [480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800]
+    # scales = [480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800]
+    scales = [64]
 
     if image_set == "train":
         return T.Compose(
@@ -169,7 +170,8 @@ def make_coco_transforms(image_set):
         )
 
     if image_set == "val":
-        return T.Compose([T.RandomResize([800], max_size=1333), normalize,])
+        # return T.Compose([T.RandomResize([800], max_size=1333), normalize,])
+        return T.Compose([T.RandomResize([64], max_size=1333), normalize,])
 
     raise ValueError(f"unknown {image_set}")
 
